df2db5e5572dabbced9d4adb35e2fae7b4d9973e
Chhavee-s <132134966+Chhavee-s@users.noreply.github.com>
2023-10-09 12:21:06 +0530

    Create ContactCardsModel.java

---
+++ aemsite/core/src/main/java/com/aemsite/demo/core/models/ContactCardsModel.java
@@ -0,0 +1,26 @@
+
+package com.aemsite.demo.core.models;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.resource.Resource;
+
+@Model(adaptables = Resource.class, resourceType= ContactCardsModel.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name="jackson", selector="test", extensions="json")
+public class ContactCardsModel {
+
+    static final String RESOURCE_TYPE="/apps/aemsite/components/multiField";
+
+    @ChildResource(name="contact")
+    public List<Contact> contact;
+
+    public List<Contact> getContact()
+    {
+        return contact;
+    }
+}
